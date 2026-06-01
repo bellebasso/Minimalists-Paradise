199c20aa3bf6f82f54fa11ba5ec5c8f292626aca
bellebasso <bellebasso@users.noreply.github.com>
2026-06-01 19:59:17 -0300

    chore: edit bundle script

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -7,9 +7,9 @@ const OUTPUT_FILE = path.join(__dirname, '..', 'theme.css');
 function bundle(entryFile) {
   const content = fs.readFileSync(entryFile, 'utf8');
   
-  // Simple regex to find @import statements
-  // This matches @import "./path/to/file.css";
-  const importRegex = /@import\s+["'](.+?)["'];/g;
+  // Robust regex to find @import statements
+  // Matches @import "file.css"; @import url("file.css"); @import url(file.css); etc.
+  const importRegex = /@import\s+(?:url\s*\(\s*)?["']?([^"']+)["']?\s*\)?\s*;/g;
   
   let bundledContent = content.replace(importRegex, (match, importPath) => {
     const fullPath = path.resolve(path.dirname(entryFile), importPath);
